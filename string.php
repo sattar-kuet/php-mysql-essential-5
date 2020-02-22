c46a22891250f4469d6ff2634b68aa2ee5311d0d
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 11:11:43 +0600

    first commit

---
+++ string.php
@@ -0,0 +1,17 @@
+<?php
+$author ='Abdus Sattar';
+$course ='PHP with MySql Essential Training';
+
+echo 'Hello'.' world';
+
+echo $course.' By '.$author;
+
+echo "$course";
+
+echo '$course';
+
+echo "$course by $author";
+
+echo "{$course} by {$author}";
+
+?>
